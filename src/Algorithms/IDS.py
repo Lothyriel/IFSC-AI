d9cd36ea83b9430be04c1c8ddd4828427a2df0d5
João Xavier <fastjonh@gmail.com>
2021-11-17 07:55:10 -0300

    att

--- src/Algorithms/IDS.py
+++ src/Algorithms/IDS.py
@@ -1,8 +1,31 @@
+from src.Domain.Exceptions import IDSMaxDepth, EmptyBorder
 from src.Domain.Node import Node
 from src.Domain.Search import Search
 
 
 class IDS(Search):
-    def search(self) -> list[Node]:
-        pass
+    def search(self) -> list[Node]:  # busca padrao em grafo
+        if not self.iddfs(self.root, self.destiny, 100):
+            raise IDSMaxDepth
+        return self.back_tracking()
 
+    def dls(self, src, target, max_depth):
+        self.search_path.append(src)
+        if src in target:
+            return True
+
+        if max_depth <= 0:
+            self.search_path = []
+            return False
+
+        for i in self.graph[src]:
+            i.parent = src
+            if self.dls(i, target, max_depth - 1):
+                return True
+        return False
+
+    def iddfs(self, src, target, max_depth):
+        for i in range(max_depth):
+            if self.dls(src, target, i):
+                return True
+        return False
--- src/Domain/Exceptions.py
+++ src/Domain/Exceptions.py
@@ -1,3 +1,6 @@
 class EmptyBorder(Exception):
     pass
 
+
+class IDSMaxDepth(Exception):
+    pass
