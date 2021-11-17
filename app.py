2369058dd2f2bedf138e81e4294c02e47395d4ac
João Xavier <fastjonh@gmail.com>
2021-11-17 00:03:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from src.Extensions.GraphHelper import GraphHelper
 from src.Extensions.GraphTransformer import get_matrix_data, GraphTransformer
 
 if __name__ == '__main__':  # inicializando o grafo com uma matriz guardada no arquivo csv local
-    node_matrix = get_matrix_data("..\\csv\\armazem.csv")
+    try:
+        node_matrix = get_matrix_data("..\\csv\\armazem.csv")
+    except Exception:
+        raise Exception("buceta caralho inferno")
+
     graph = GraphTransformer(node_matrix).create_graph()
     helper = GraphHelper(graph, node_matrix)
     init_api(helper)
